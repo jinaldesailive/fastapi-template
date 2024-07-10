fa1c4567f77dc29897ad8fcdbb5a9f3e5fb3c5b7
Jinal Desai <58817990+jinaldesailive@users.noreply.github.com>
2024-07-10 17:13:02 +0100

    updated

--- content/main.py
+++ content/main.py
@@ -12,6 +12,6 @@ async def root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    config = uvicorn.Config("main:app", log_level="info")
+    config = uvicorn.Config("main:app", port=8000, log_level="info")
     server = uvicorn.Server(config)
     server.run()
